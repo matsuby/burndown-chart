40216e0f450bc1d5be9a4363a0349d7f264f94ae
matsuby <matsubygh@gmail.com>
2020-03-10 01:11:36 +0900

    setup typescript/eslint/prettier

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        singleQuote: true,
+        trailingComma: "es5"
+      }
+    ]
+  }
+};
